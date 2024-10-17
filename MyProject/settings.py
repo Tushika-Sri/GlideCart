d2c1ea9e309c16e142d3202e60be61ba336fd5ed
Tushika Srivastava <tushika@BHAVYAs-MacBook-Air.local>
2024-10-17 19:09:49 +0530

    settings domain update

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#mxrh@!cl+(w72t^wv65n+)e8$ibbig71wfjv#6e)(gfjaj52u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['GlideCart.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['glidecart.onrender.com','GlideCart.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
